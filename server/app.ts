3926b084677acfffcfbfe26fd6606daea1a474b4
Shreshth Verma <verma2007s@gmail.com>
2024-04-11 10:01:53 +0530

    feat: added router and register route

--- server/app.ts
+++ server/app.ts
@@ -3,13 +3,18 @@ import 'dotenv/config'
 import pinoHttp from 'pino-http'
 import logger from './config/logger'
 import instantiateModels from './schema/index'
+import router from './routes'
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(pinoHttp({ logger, useLevel: 'debug' }))
+app.use(pinoHttp({ logger, useLevel: 'trace' }))
+app.use('/v1', router)
 
 instantiateModels()
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 export default app
