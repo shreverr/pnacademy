3d8310517d323c5045ebb9d7ccae97f1ea9018e6
Shreshth Verma <verma2007s@gmail.com>
2024-08-08 23:35:56 +0530

    fix: app crashing on json parse error

--- server/app.ts
+++ server/app.ts
@@ -4,14 +4,14 @@ import pinoHttp from "pino-http";
 import logger from "./config/logger";
 import instantiateModels from "./schema/index";
 import router from "./routes";
-import { handleError } from "./middleware/error";
+import { handleError, jsonParseError } from "./middleware/error";
 import cors from 'cors'
 
 const app = express();
 
 app.use(cors())
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }))
+app.use(jsonParseError)
 app.use(pinoHttp({ logger, useLevel: "trace" }));
 app.use("/v1", router);
 app.use(handleError);
--- server/middleware/error.ts
+++ server/middleware/error.ts
@@ -5,3 +5,11 @@ import { AppError } from "../lib/appError";
 export const handleError: ErrorRequestHandler = async (err: AppError, req: Request, res: Response, next: NextFunction) => {
   handler.handleError(err, res)
 }
+
+export const jsonParseError = (err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof SyntaxError && 'status' in err && err.status === 400 && 'body' in err) {
+    throw new AppError('Invalid JSON payload passed.', 400, 'JSON syntax error', true)
+    next(err)
+  }
+  next();
+}
