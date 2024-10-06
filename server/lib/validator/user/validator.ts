a4e05d98126acfa17012a6aff780f315c893c907
animesh <animeshchaudhri3@gmail.com>
2024-10-07 00:00:45 +0530

    Feat: User password reset and admin password management

--- server/lib/validator/user/validator.ts
+++ server/lib/validator/user/validator.ts
@@ -168,6 +168,49 @@ export const validateNewAccessToken = [
     .withMessage("Must be a valid JWT token"),
 ];
 
+export const validateUserUpdatePassword = [
+  check("password").notEmpty().isStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    returnScore: false,
+    pointsPerUnique: 1,
+    pointsPerRepeat: 0,
+    pointsForContainingLower: 10,
+    pointsForContainingUpper: 10,
+    pointsForContainingNumber: 10,
+    pointsForContainingSymbol: 10,
+  }).withMessage(`Password must be more than 8 characters long with
+        at least 1 lowercase, 1 uppercase, 1 number and 1 symbol`),
+];
+
+export const validateAdminPasswordReset = [
+  check("userId")
+    .not()
+    .isEmpty()
+    .isUUID(4)
+    .escape()
+    .withMessage("Last name must be more than 2 characters long"),
+
+  check("password").notEmpty().isStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    returnScore: false,
+    pointsPerUnique: 1,
+    pointsPerRepeat: 0,
+    pointsForContainingLower: 10,
+    pointsForContainingUpper: 10,
+    pointsForContainingNumber: 10,
+    pointsForContainingSymbol: 10,
+  }).withMessage(`Password must be more than 8 characters long with
+          at least 1 lowercase, 1 uppercase, 1 number and 1 symbol`),
+];
+
 export const validateUserUpdate = [
   check("id")
     .not()
@@ -201,11 +244,7 @@ export const validateUserUpdate = [
     .isLength({ min: 10, max: 10 })
     .withMessage("Phone number must be a valid phone number"),
 
-  check("roleId")
-  .optional()
-  .notEmpty()
-  
-  
+  check("roleId").optional().notEmpty(),
 ];
 
 export const validateUserRole = [
